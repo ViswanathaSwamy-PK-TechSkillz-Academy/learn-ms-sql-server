420d05b788077264e174b62279061a458a3f9268
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-17 23:39:15 +0530

    Changes

--- BooksDataStore/SeedData/books-data.sql
+++ BooksDataStore/SeedData/books-data.sql
@@ -4,7 +4,7 @@ BEGIN
    DELETE [dbo].[Books];
 
 	INSERT INTO [dbo].[Books] 
-		([Title], [Author], [PublishedOn], [ISBN], [Description], [Price], [ImageUrl])
+		([Title], [Author], [PublishedOn], [ISBN], [Description], [Price], [PictureUrl])
 	VALUES 
 		('Book 1', 'Sri Varu', '1988-01-01', '978-0061122415', 'Book 1 Description', 12.34, 'https://placehold.co/600x400/png')
 		, ('Book 2', 'Sri Varu', '1998-01-01', '978-0061122415', 'Book 2 Description', 12.34, 'https://placehold.co/600x400/png');
