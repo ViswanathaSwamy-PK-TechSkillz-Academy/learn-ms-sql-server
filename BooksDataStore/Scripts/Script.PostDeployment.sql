0148c7db34b4484e3d885092570f284c8fe7c098
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-18 11:17:30 +0530

    Swamy/17feb work (#1)
    * Initial Version
    
    * Create Books.sql
    
    * Initial Version
    
    * Delete books-data.sql
    
    * Changes
    
    * Changes

---
+++ BooksDataStore/Scripts/Script.PostDeployment.sql
@@ -0,0 +1,21 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+PRINT 'Starting the Post Deployment Scripts for BooksDataStore'
+GO
+
+-- Books Data
+:r "../SeedData/books-data.sql"
+GO
+
+PRINT 'Completed the Post Deployment Scripts for BooksDataStore'
+GO
---
+++ BooksDataStore/SeedData/books-data.sql
@@ -0,0 +1,12 @@
+﻿
+IF EXISTS(SELECT 1 FROM [dbo].[Books])
+BEGIN
+   DELETE [dbo].[Books];
+
+	INSERT INTO [dbo].[Books] 
+		([Title], [Author], [PublishedOn], [ISBN], [Description], [Price], [PictureUrl])
+	VALUES 
+		('Book 1', 'Sri Varu', '1988-01-01', '978-0061122415', 'Book 1 Description', 12.34, 'https://placehold.co/600x400/png')
+		, ('Book 2', 'Sri Varu', '1998-01-01', '978-0061122415', 'Book 2 Description', 12.34, 'https://placehold.co/600x400/png');
+END
+
