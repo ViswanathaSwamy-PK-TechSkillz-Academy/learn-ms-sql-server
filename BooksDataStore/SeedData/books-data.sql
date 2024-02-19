defa9255a06d6fabf4ec473c145e4a79fc88abff
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-19 09:17:56 +0530

    Swamy/17feb work (#2)
    * Initial Version
    
    * Create Books.sql
    
    * Initial Version
    
    * Delete books-data.sql
    
    * Changes
    
    * Changes
    
    * Update books-data.sql

--- BooksDataStore/SeedData/books-data.sql
+++ BooksDataStore/SeedData/books-data.sql
@@ -1,12 +1,20 @@
-﻿
-IF EXISTS(SELECT 1 FROM [dbo].[Books])
+﻿IF EXISTS(SELECT 1 FROM [dbo].[Books])
 BEGIN
+
+   PRINT 'Found [dbo].[Books] and deleting it ...'
+
    DELETE [dbo].[Books];
 
-	INSERT INTO [dbo].[Books] 
-		([Title], [Author], [PublishedOn], [ISBN], [Description], [Price], [PictureUrl])
-	VALUES 
-		('Book 1', 'Sri Varu', '1988-01-01', '978-0061122415', 'Book 1 Description', 12.34, 'https://placehold.co/600x400/png')
-		, ('Book 2', 'Sri Varu', '1998-01-01', '978-0061122415', 'Book 2 Description', 12.34, 'https://placehold.co/600x400/png');
 END
+GO
 
+
+PRINT 'Inserting the seed data into [dbo].[Books] table ...'
+
+INSERT INTO [dbo].[Books] 
+	([Title], [Author], [PublishedOn], [ISBN], [Description], [Price], [Pages], [PictureUrl])
+VALUES 
+	('Book 1', 'Sri Varu', '1988-01-01', '978-0061122415', 'Book 1 Description', 54.45, 101, 'https://placehold.co/600x400/png')
+	, ('Book 2', 'Sri Varu', '1998-01-01', '978-0061122415', 'Book 2 Description', 18.99, 202, 'https://placehold.co/600x400/png');
+
+PRINT 'Completed inserting the seed data into [dbo].[Books] table !!'
